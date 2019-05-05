57f4dbda21e2941b4ff7bcd53123f3aa9edd0807
Danielle Parkinson <dparkinson2@hotmail.co.uk>
2019-05-05 16:55:31 +0100

    Include javascript to fix form issues

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 var scrollButton = document.getElementById('scroll')
 var guestSelect = document.getElementById('guests-number')
+var rsvpForm = document.getElementById('rsvp')
 
 window.addEventListener('scroll', function() {
   var scrollTop = window.scrollY
@@ -24,13 +25,34 @@ if (guestSelect) {
   guestSelect.addEventListener('change', function(event) {
     var guestFields = Array.from(document.querySelectorAll('.guest-fields'))
     var selected = event.currentTarget.selectedIndex
+    var guestNameSelector = 'input[name$="Guest Name"]'
 
     guestFields.forEach(function(guestField, index) {
       if (index <= selected) {
         guestField.classList.add('active')
+        guestField
+          .querySelector(guestNameSelector)
+          .setAttribute('required', 'required')
       } else {
         guestField.classList.remove('active')
+        guestField.querySelector(guestNameSelector).removeAttribute('required')
       }
     })
   })
 }
+
+rsvpForm.addEventListener('change', function(event) {
+  var dietaryRequirementSelected = event.target
+
+  if (dietaryRequirementSelected.className.includes('dietary-checkbox')) {
+    var textField = dietaryRequirementSelected.parentElement.getElementsByClassName(
+      'dietary-text-field'
+    )[0]
+
+    if (dietaryRequirementSelected.checked) {
+      textField.setAttribute('required', 'required')
+    } else {
+      textField.removeAttribute('required')
+    }
+  }
+})
