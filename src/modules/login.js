7c6bb84ebfe72f23259682940316787980c54b9c
Danielle Parkinson <dparkinson2@hotmail.co.uk>
2019-05-12 12:41:53 +0100

    Add dummy login hook

---
+++ src/modules/login.js
@@ -0,0 +1,11 @@
+import {useState} from 'react'
+
+export function useLogin() {
+  var [loggedIn, setLoggedInStatus] = useState(false)
+
+  function login() {
+    setLoggedInStatus(true)
+  }
+
+  return {loggedIn, login}
+}
