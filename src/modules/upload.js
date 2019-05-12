906f8e62fcb1f92bb7d1e016134be9baf89badb1
Ben Styles <enquiries@leeds-ebooks.co.uk>
2019-05-12 15:15:04 +0100

    Move firebase init into separate module

--- src/modules/upload.js
+++ src/modules/upload.js
@@ -1,17 +1,5 @@
 import {useState, useEffect} from 'react'
-import firebase from 'firebase/app'
-import 'firebase/storage'
-import 'firebase/firestore'
-
-firebase.initializeApp({
-  apiKey: 'AIzaSyAlWGSlrpRs6_J9pDRjwsTw08n62PorAEc',
-  authDomain: 'tansy-and-jim.firebaseapp.com',
-  databaseURL: 'https://tansy-and-jim.firebaseio.com',
-  projectId: 'tansy-and-jim',
-  storageBucket: 'tansy-and-jim.appspot.com',
-  messagingSenderId: '456539222310',
-  appId: '1:456539222310:web:cec105ffae223494',
-})
+import firebase from './firebase'
 
 var storage = firebase
   .storage()
