a6dee27fd3077f6b0de949a4d09893dd0461e876
Ben Styles <enquiries@leeds-ebooks.co.uk>
2019-05-13 21:10:11 +0100

    Watch for image uploads

--- src/modules/upload.js
+++ src/modules/upload.js
@@ -8,41 +8,31 @@ var storage = firebase
 
 var database = firebase.firestore().collection('gallery')
 
-function retrieve() {
-  return database.get().then(function(querySnapshot) {
-    return Promise.all(
-      querySnapshot.docs
-        .map(function(docSnapshot) {
-          return docSnapshot.get('name')
-        })
-        .map(function(name) {
-          return storage.child(name).getDownloadURL()
-        })
-    )
-  })
-}
-
 export function useRemoteImages() {
   var [imageUrls, setImageUrls] = useState([])
 
-  useEffect(() => {
-    retrieve().then(function(urls) {
-      setImageUrls(urls)
-    })
-  }, [])
+  useEffect(
+    () =>
+      database.onSnapshot({
+        next: querySnapshot => {
+          Promise.all(
+            querySnapshot.docs
+              .map(docSnapshot => docSnapshot.get('name'))
+              .map(name => storage.child(name).getDownloadURL())
+          ).then(setImageUrls)
+        },
+      }),
+    [setImageUrls]
+  )
 
   return imageUrls
 }
 
 export function upload(files) {
-  Promise.all(
-    Array.from(files).map(function(file) {
-      return Promise.all([
-        storage.child(file.name).put(file, {contentType: file.type}),
-        database.add({name: file.name}),
-      ])
-    })
-  ).then(function() {
-    window.location.reload()
-  })
+  Array.from(files).map(file =>
+    storage
+      .child(file.name)
+      .put(file, {contentType: file.type})
+      .then(() => database.add({name: file.name}))
+  )
 }
